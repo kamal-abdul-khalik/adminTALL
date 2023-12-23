fed6c3344b8c31533ce810888157f7d94609d53d
kamal-abdul-khalik <elsakcf@gmail.com>
2023-12-23 20:07:30 +0800

    add volt & toogle theme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,61 +9,68 @@
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
 
-    <body>
+    <body class="font-sans antialiased" x-data="{ darkMode: false }" x-init="if (!('darkMode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        localStorage.setItem('darkMode', JSON.stringify(true));
+    }
+    darkMode = JSON.parse(localStorage.getItem('darkMode'));
+    $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))" x-cloak>
 
-        <div x-data="{ sidebarOpen: false }">
-            <div class="flex min-h-screen">
-
-                @auth
-                    <!-- regular sidebar -->
-                    <div class="flex-none hidden w-full px-4 sidebar md:block md:w-60 bg-primary dark:bg-slate-900">
-                        @include('layouts.navigation')
-                    </div>
-
-                    <!--sidebar on mobile-->
-                    <div x-show="sidebarOpen" class="min-w-full px-4 sidebar bg-primary dark:bg-slate-900 md:hidden">
-                        @include('layouts.navigation')
-                    </div>
-                @endauth
-
-                <div id="main" class="w-full bg-gray-100 dark:bg-slate-800">
+        <div x-bind:class="{ 'dark': darkMode === true }" class="min-h-screen bg-gray-100">
+            <div x-data="{ sidebarOpen: false }">
+                <div class="flex min-h-screen">
 
                     @auth
-                        <div class="flex justify-between px-2 py-2 mb-5 bg-white top-12 dark:bg-slate-900">
+                        <!-- regular sidebar -->
+                        <div class="flex-none hidden w-full px-4 sidebar md:block md:w-60 bg-primary dark:bg-slate-900">
+                            @include('layouts.navigation')
+                        </div>
 
-                            <div class="flex">
-                                <button @click.stop="sidebarOpen = !sidebarOpen"
-                                    class="pl-1 pr-2 md:hidden focus:outline-none">
-                                    <svg class="w-6 text-gray-900 transition duration-150 ease-in-out dark:text-gray-200"
-                                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M4 6h16M4 12h8m-8 6h16" />
-                                    </svg>
-                                </button>
-
-                                <livewire:admin.search />
-
-                            </div>
-
-                            <div class="flex">
-                                <livewire:admin.notifications-menu />
-                                <livewire:admin.help-menu />
-                                <livewire:admin.users.user-menu />
-                            </div>
+                        <!--sidebar on mobile-->
+                        <div x-show="sidebarOpen" class="min-w-full px-4 sidebar bg-primary dark:bg-slate-900 md:hidden">
+                            @include('layouts.navigation')
                         </div>
                     @endauth
 
-                    <div class="py-5 px-7">
-                        {{ $slot ?? '' }}
+                    <div id="main" class="w-full bg-gray-100 dark:bg-slate-800">
+
+                        @auth
+                            <div class="flex justify-between px-2 py-2 mb-5 bg-white top-12 dark:bg-slate-900">
+
+                                <div class="flex">
+                                    <button @click.stop="sidebarOpen = !sidebarOpen"
+                                        class="pl-1 pr-2 md:hidden focus:outline-none">
+                                        <svg class="w-6 text-gray-900 transition duration-150 ease-in-out dark:text-gray-200"
+                                            fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M4 6h16M4 12h8m-8 6h16" />
+                                        </svg>
+                                    </button>
+
+                                    <livewire:admin.search />
+
+                                </div>
+
+                                <div class="flex items-center gap-x-2">
+                                    <livewire:admin.toogle-theme />
+                                    <livewire:admin.notifications-menu />
+                                    <livewire:admin.help-menu />
+                                    <livewire:admin.users.user-menu />
+                                </div>
+                            </div>
+                        @endauth
+
+                        <div class="py-5 px-7">
+                            {{ $slot ?? '' }}
+                        </div>
                     </div>
+
+                </div>
+
+                <div class="flex justify-between p-5 text-xs bg-white dark:bg-slate-900 dark:text-gray-300">
+                    <div>{{ __('Copyright') }} &copy; {{ date('Y') }} {{ config('app.name') }}</div>
                 </div>
 
             </div>
-
-            <div class="flex justify-between p-5 text-xs bg-white dark:bg-slate-900 dark:text-gray-300">
-                <div>{{ __('Copyright') }} &copy; {{ date('Y') }} {{ config('app.name') }}</div>
-            </div>
-
         </div>
 
     </body>
--- resources/views/livewire/admin/notifications-menu.blade.php
+++ resources/views/livewire/admin/notifications-menu.blade.php
@@ -1,34 +1,45 @@
 <div x-data="{ isOpen: false }">
     @can('view_notifications')
 
-        <button wire:click="open" @click="isOpen = !isOpen" class="focus:outline-none pt-2">
-            <div class="relative">
+        <button wire:click="open" @click="isOpen = !isOpen" class="pt-2 focus:outline-none">
+            <div class="relative pl-2">
                 @if ($unseenCount > 0)
-                    <span class="bg-red-500 absolute top-0 left-4 block h-4 w-4 rounded-full ring-2 ring-white text-xs text-white" aria-hidden="true">{{ $unseenCount }}</span>
+                    <span
+                        class="absolute top-0 block w-4 h-4 text-xs text-white bg-red-500 rounded-full left-4 ring-2 ring-white"
+                        aria-hidden="true">{{ $unseenCount }}</span>
                 @endif
 
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5" />
                 </svg>
+
             </div>
         </button>
 
-        <div x-show.transition="isOpen" class="fixed z-50 inset-0 overflow-hidden" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
-            <div class="absolute inset-0 overflow-hidden bg-gray-500 bg-opacity-75 transition-opacity">
+        <div x-show.transition="isOpen" class="fixed inset-0 z-50 overflow-hidden" aria-labelledby="slide-over-title"
+            role="dialog" aria-modal="true">
+            <div class="absolute inset-0 overflow-hidden transition-opacity bg-gray-500 bg-opacity-75">
 
-                <div class="fixed inset-y-0 right-0 pl-10 max-w-full flex sm:pl-16">
+                <div class="fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
 
                     <div class="w-screen max-w-md">
-                        <div class="h-full flex flex-col bg-white dark:bg-gray-700 dark:text-gray-300 shadow-xl overflow-y-scroll">
+                        <div
+                            class="flex flex-col h-full overflow-y-scroll bg-white shadow-xl dark:bg-gray-700 dark:text-gray-300">
 
                             <div class="p-6">
                                 <div class="flex items-start justify-between">
-                                    <h2 class="text-lg mb-0 font-medium text-gray-900 dark:text-gray-300">{{ __('Notifications') }}</h2>
-                                    <div class="ml-3 flex items-center">
-                                        <button @click="isOpen = !isOpen" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-indigo-500">
+                                    <h2 class="mb-0 text-lg font-medium text-gray-900 dark:text-gray-300">
+                                        {{ __('Notifications') }}</h2>
+                                    <div class="flex items-center ml-3">
+                                        <button @click="isOpen = !isOpen"
+                                            class="text-gray-400 bg-white rounded-md hover:text-gray-500 focus:ring-2 focus:ring-indigo-500">
                                             <span class="sr-only">{{ __('Close panel') }}</span>
-                                            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                            <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    d="M6 18L18 6M6 6l12 12" />
                                             </svg>
                                         </button>
                                     </div>
@@ -37,38 +48,44 @@
 
                             <div class="border-b border-gray-200"></div>
 
-                            <ul class="flex-1 divide-y divide-gray-200 overflow-y-auto">
+                            <ul class="flex-1 overflow-y-auto divide-y divide-gray-200">
 
                                 @if (count($notifications) === 0)
                                     <li class="p-6">{{ __('No notifications yet.') }}</li>
                                 @else
-                                    @foreach($notifications as $notification)
-                                    <li class="px-6 py-5 relative">
-                                        <div class="group flex justify-between items-center">
-                                            @if (!empty($notification->link))
-                                                <a wire:navigate href="{{ $notification->link }}" class="-m-1 p-1 block">
-                                            @endif
+                                    @foreach ($notifications as $notification)
+                                        <li class="relative px-6 py-5">
+                                            <div class="flex items-center justify-between group">
+                                                @if (!empty($notification->link))
+                                                    <a wire:navigate href="{{ $notification->link }}"
+                                                        class="block p-1 -m-1">
+                                                @endif
 
-                                                <div class="absolute inset-0 group-hover:bg-gray-50 dark:group-hover:bg-gray-500" aria-hidden="true"></div>
+                                                <div class="absolute inset-0 group-hover:bg-gray-50 dark:group-hover:bg-gray-500"
+                                                    aria-hidden="true"></div>
 
-                                                <div class="flex-1 flex items-center min-w-0 relative">
+                                                <div class="relative flex items-center flex-1 min-w-0">
 
-                                                    <span class="flex-shrink-0 inline-block relative">
+                                                    <span class="relative flex-shrink-0 inline-block">
                                                         @if (!empty($notification->assignedFrom->image))
-                                                            <img class="h-10 w-10 rounded-full" src="{{ storage_url($notification->assignedFrom->image) }}" alt="{{ $notification->assignedFrom->name }}">
+                                                            <img class="w-10 h-10 rounded-full"
+                                                                src="{{ storage_url($notification->assignedFrom->image) }}"
+                                                                alt="{{ $notification->assignedFrom->name }}">
                                                         @endif
                                                     </span>
 
                                                     <div class="ml-4">
-                                                        <p class="text-sm font-medium text-gray-900 dark:text-gray-200">{{ $notification->title }}</p>
-                                                        <p class="text-sm text-gray-500 dark:text-gray-200">{{ $notification->created_at->diffForHumans() }}</p>
+                                                        <p class="text-sm font-medium text-gray-900 dark:text-gray-200">
+                                                            {{ $notification->title }}</p>
+                                                        <p class="text-sm text-gray-500 dark:text-gray-200">
+                                                            {{ $notification->created_at->diffForHumans() }}</p>
                                                     </div>
                                                 </div>
-                                            @if (!empty($notification->link))
-                                                </a>
-                                            @endif
-                                        </div>
-                                    </li>
+                                                @if (!empty($notification->link))
+                                                    </a>
+                                                @endif
+                                            </div>
+                                        </li>
                                     @endforeach
                                 @endif
                             </ul>
