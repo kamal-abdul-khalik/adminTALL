5fcb135f54715f099e40c3fb5830eeae675a4290
kamal-abdul-khalik <elsakcf@gmail.com>
2023-12-24 21:15:12 +0800

    wip

--- resources/views/components/form/daterange.blade.php
+++ resources/views/components/form/daterange.blade.php
@@ -7,7 +7,6 @@
 ])
 
 @if ($label === 'none')
-
 @elseif ($label === '')
     @php
         //remove underscores from name
@@ -22,29 +21,30 @@
 @endif
 
 @php
-    $options = array_merge([
-        'dateFormat' => 'd-m-Y',
-        'enableTime' => false,
-        'time_24hr' => true,
-        'mode' => 'range',
-    ], $options);
+    $options = array_merge(
+        [
+            'dateFormat' => 'd-m-Y',
+            'enableTime' => false,
+            'time_24hr' => true,
+            'mode' => 'range',
+        ],
+        $options,
+    );
 @endphp
 
 <div class="mb-5">
-    @if ($label !='none')
-        <label for="{{ $name }}" class="block text-sm font-medium leading-5 text-gray-700 dark:text-gray-200">{{ $label }} @if ($required != '') <span class="error">*</span>@endif</label>
+    @if ($label != 'none')
+        <label for="{{ $name }}"
+            class="block mb-3 text-sm font-medium leading-5 text-gray-700 dark:text-gray-200">{{ $label }}
+            @if ($required != '')
+                <span class="error">*</span>
+            @endif
+        </label>
     @endif
     <div class="mt-1 rounded-md shadow-sm">
-        <input
-            x-data
-            x-init="flatpickr($refs.input, {{json_encode((object)$options)}});"
-            x-ref="input"
-            type="text"
-            id="{{ $name }}"
-            name="{{ $name }}"
-            value="{{ $slot }}"
-            {{ $required }}
-            {{ $attributes->merge(['class' => 'mt-1 block w-full dark:bg-gray-500 dark:text-gray-200 dark:placeholder-gray-200 border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-light-blue-500 focus:border-light-blue-500 sm:text-sm']) }}>
+        <input x-data x-init="flatpickr($refs.input, {{ json_encode((object) $options) }});" x-ref="input" type="text" id="{{ $name }}"
+            name="{{ $name }}" value="{{ $slot }}" {{ $required }}
+            {{ $attributes->merge(['class' => 'w-full rounded-lg border border-gray-300 dark:border-slate-700 py-2 px-3 outline-0 dark:bg-slate-900 transition duration-200 focus:border-blue-300 focus:ring-4 focus:ring-indigo-100 focus:dark:ring-blue-800 dark:text-slate-300 sm:text-sm']) }}>
         @error($name)
             <p class="error">{{ $message }}</p>
         @enderror
